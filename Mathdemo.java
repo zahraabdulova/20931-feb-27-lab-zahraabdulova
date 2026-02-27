f789fb128ddd30f38fcfc9847f3e0cd7cc991a58
zahraabdulova <zabdulova16110@ada.edu.az>
2026-02-27 13:30:54 +0400

    Create Mathdemo.java

---
+++ Mathdemo.java
@@ -0,0 +1,16 @@
+public class MathDemo {
+
+    public int min(int a, int b) {
+        if (a < b) {
+            return a;
+        }
+        return b;
+    }
+
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+}
